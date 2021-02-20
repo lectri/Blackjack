e0c955709d76e20a83a00d9611ee74377c12705c
lectri <cmade2006@gmail.com>
2021-02-20 18:33:49 -0500

    Added main

---
+++ main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> cards = new List<int>();
+            for (int i = 2; i == 13; i++)
+            {
+
+                for (int j = 0; i == 13; i++)
+                {
+                    cards.Add(i);
+                }
+
+            }
+
+            foreach (int i in cards)
+            {
+                Console.Write(i + " ");
+            }
+
+
+        }
+    }
+}
